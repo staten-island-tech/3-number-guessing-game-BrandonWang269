d41637316c41187fbe32ff94fa1bda5a05d04d11
BrandonWang269 <brandonw269@nycstudents.net>
2026-02-12 10:13:33 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-x = int(input(1 <= y <= 10))
+import random
+random_number = random.randint(1, 10)
+while True:
+    guess_str = input("Guess the secret number (between 1 and 10): ")
+
+    guess = int(guess_str)
+
+    if guess == random_number:
+        print("Correct")
+    else:
+        print("wrong")
+
+        
